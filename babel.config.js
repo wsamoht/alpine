899c1b7a08c079c91d674b33a2d24687eca18879
twestrick <twestrick@mediagrid.us>
2020-01-29 10:28:02 -0500

    Add support for IE 11

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,8 @@ module.exports = {
             {
                 targets: {
                     node: 'current',
-                    edge: '18'
+                    edge: '18',
+                    ie: '11'
                 },
             },
         ],
